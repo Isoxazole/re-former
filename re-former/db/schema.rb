8c429c0ecfa454bff10d1aaa45ef3117af0f84a0
Isoxazole <wmorris1367@gmail.com>
2019-12-08 13:52:10 -0700

    decided not to use scaffolding for this

--- re-former/db/schema.rb
+++ re-former/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_194959) do
+ActiveRecord::Schema.define(version: 2019_12_08_205029) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
